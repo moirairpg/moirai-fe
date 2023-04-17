01d65afd339a271ff859ad69c584dfc72da19f27
Thales Alves <thales@thalesalv.es>
2023-04-18 00:36:46 +0100

    fixed problem with adding/editing lorebook

--- src/views/lorebook/Lorebooks.vue
+++ src/views/lorebook/Lorebooks.vue
@@ -65,7 +65,7 @@ const hideEntryDialog = () => {
 
 const saveLorebook = async () => {
     lorebookSubmitted.value = true;
-    if (entry.value.name.trim() && entry.value.description.trim() && entry.value.visibility.trim()) {
+    if (lorebook.value.name.trim() && lorebook.value.description.trim() && lorebook.value.visibility.value.trim()) {
         if (lorebook.value.id) {
             try {
                 lorebook.value.visibility = lorebook.value.visibility.value ? lorebook.value.visibility.value : lorebook.value.visibility;
