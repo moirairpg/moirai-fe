2eb3b5a22fdf89210268b279b57cb8f16c2ff8a2
Thales Alves <thp@vizrt.com>
2023-04-18 18:18:44 +0100

    created world service

---
+++ src/service/WorldService.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+
+const baseUrl = import.meta.env.VITE_CHATRPG_API_BASEURL;
+
+export default class WorldService {
+
+    async getAllWorlds() {
+        try {
+            const response = await axios.get(`${baseUrl}/world`)
+            return await response.data.worlds
+        } catch (error) {
+            console.error(`Error retrieving world data -> ${error}`)
+            throw error
+        }
+    }
+
+    async createWorld(world) {
+        try {
+            const newWorld = await axios.post(`${baseUrl}/world`, world)
+            return newWorld.data.world
+        } catch (error) {
+            console.error(`Error creating world -> ${error}`)
+            throw error
+        }
+    }
+
+    async updateWorld(world) {
+        try {
+            const response = await axios.put(`${baseUrl}/world/${world.id}`, world)
+            return await response.data
+        } catch (error) {
+            console.error(`Error updating world with id ${world.id} -> ${error}`)
+            throw error
+        }
+    }
+
+    async deleteWorld(world) {
+        try {
+            await axios.delete(`${baseUrl}/world/${world.id}`, world)
+        } catch (error) {
+            console.error(`Error deleting world with id ${world.id} -> ${error}`)
+            throw error
+        }
+    }
+}
