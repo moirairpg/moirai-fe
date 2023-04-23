40ce38b1a6b2ddf0324e0948d3432860f9b509a1
Thales Alves <thaalesalves@users.noreply.github.com>
2023-04-23 01:19:47 +0100

    Feature/card views (#3)
    * made theme switch better; changed loreboko page to cards
    
    * removed hashtag from url
    
    * implemented login with discord and redirect to initial page if not logged in

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 import PrimeVue from 'primevue/config';
 import AutoComplete from 'primevue/autocomplete';
@@ -107,6 +108,7 @@ import '@/assets/styles.scss';
 
 const app = createApp(App);
 
+app.use(store);
 app.use(router);
 app.use(PrimeVue, { ripple: true });
 app.use(ToastService);
