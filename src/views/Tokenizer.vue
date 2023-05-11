15ebb8f73175a32c6c5a9b02b86b88f601f2acec
Thales Alves <thales@thalesalv.es>
2023-05-11 23:31:00 +0100

    externalized tokenizer to its own function

--- src/views/Tokenizer.vue
+++ src/views/Tokenizer.vue
@@ -1,25 +1,14 @@
 <script setup>
-import { ref, onMounted, onBeforeMount } from 'vue';
-import { encode, decodeGenerator } from 'gpt-tokenizer';
+import { ref } from 'vue';
+import decodeTokens from '../resources/tokenizer';
 
 const text = ref(null);
-const decodedTokens = ref([]);
-const encodedTokens = ref(null);
-
-onBeforeMount(() => {});
-
-onMounted(() => {});
+const processedTokens = ref(null);
 
 const pastelColors = ref(['rgba(107,64,216,.3)', 'rgba(104,222,122,.4)', 'rgba(244,172,54,.4)', 'rgba(239,65,70,.4)', 'rgba(39,181,234,.4)']);
 
-const decodeTokens = (event) => {
-    decodedTokens.value = [];
-    text.value = event.target.value;
-    encodedTokens.value = encode(text.value);
-
-    for (const token of decodeGenerator(encodedTokens.value)) {
-        decodedTokens.value.push(String(token).replaceAll(' ', '\u00A0').replaceAll('\n', '<newline>'));
-    }
+const processTokens = (event) => {
+    processedTokens.value = decodeTokens(event);
 };
 </script>
 
@@ -31,7 +20,7 @@ const decodeTokens = (event) => {
                 <div class="grid formgrid">
                     <div class="col-12 mb-2 lg:col-12 lg:mb-0 field">
                         <label for="text-to-tonekize">Text to be tokenized</label>
-                        <Textarea v-model="text" id="text-to-tonekize" placeholder="Text to be tokenized" rows="5" cols="30" @input="decodeTokens" />
+                        <Textarea v-model="text" id="text-to-tonekize" placeholder="Text to be tokenized" rows="5" cols="30" @input="processTokens" />
                     </div>
                     <div class="col-12 mb-2 lg:col-12 lg:mb-0 field">
                         <label for="tokenized-text">Tokenized text</label>
@@ -39,7 +28,7 @@ const decodeTokens = (event) => {
                             :style="{
                                 display: 'flex',
                                 flexWrap: 'wrap',
-                                fontFamily: monospace,
+                                fontFamily: 'monospace',
                                 width: '100%',
                                 height: '200px',
                                 overflowY: 'auto',
@@ -49,7 +38,7 @@ const decodeTokens = (event) => {
                                 alignContent: 'flex-start'
                             }"
                         >
-                            <span v-for="(token, index) in decodedTokens">
+                            <span v-for="(token, index) in processedTokens?.decodedTokens">
                                 <span
                                     :style="{
                                         backgroundColor: pastelColors[index % pastelColors.length],
@@ -64,6 +53,45 @@ const decodeTokens = (event) => {
                                 </span>
                             </span>
                         </div>
+                    </div>
+                    <div class="col-12 mb-2 lg:col-12 lg:mb-0 field">
+                        <label for="tokenized-text">Token IDs</label>
+                        <div
+                            :style="{
+                                display: 'flex',
+                                flexWrap: 'wrap',
+                                fontFamily: 'monospace',
+                                width: '100%',
+                                height: '200px',
+                                overflowY: 'auto',
+                                padding: '8px',
+                                border: '1px solid #ccc',
+                                lineHeight: '1.5',
+                                alignContent: 'flex-start'
+                            }"
+                        >
+                            <span v-for="(token, index) in processedTokens?.encodedTokens" :style="{ display: 'inline-block' }">
+                                <span
+                                    :style="{
+                                        backgroundColor: pastelColors[index % pastelColors.length],
+                                        padding: '0 0px',
+                                        marginRight: '0px',
+                                        marginBottom: '4px',
+                                        display: 'inline-block',
+                                        height: '1.5em'
+                                    }"
+                                >
+                                    [ {{ token }} ]
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid formgrid">
+                    <div class="col-12 mb-2 lg:col-12 lg:mb-0 field">
+                        <p>Tokens: {{ processedTokens?.tokens ?? 0 }}</p>
+                        <p>Characters: {{ processedTokens?.characters ?? 0 }}</p>
+                        <p>Words: {{ processedTokens?.words ?? 0 }}</p>
                     </div>
                 </div>
             </div>
