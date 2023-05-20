a93fef6778a95a49811285e44c223d2e3e54f1bf
Thales Alves <thales@thalesalv.es>
2023-05-20 02:16:04 +0100

    removed global export button

--- src/views/Personas.vue
+++ src/views/Personas.vue
@@ -246,7 +246,6 @@ const downloadPersona = () => {
                             </template>
                             <template v-slot:end>
                                 <FileUpload mode="basic" accept="image/*" :maxFileSize="1000000" label="Import" chooseLabel="Import" class="mr-2 inline-block" />
-                                <Button label="Export" icon="pi pi-upload" class="p-button-help" @click="exportCSV($event)" />
                             </template>
                         </Toolbar>
 
@@ -308,7 +307,6 @@ const downloadPersona = () => {
                             </template>
                             <template v-slot:end>
                                 <FileUpload mode="basic" accept="image/*" :maxFileSize="1000000" label="Import" chooseLabel="Import" class="mr-2 inline-block" />
-                                <Button label="Export" icon="pi pi-upload" class="p-button-help" @click="exportCSV($event)" />
                             </template>
                         </Toolbar>
 
