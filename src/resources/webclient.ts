d55836a10543945c3bfc5688b3dc52ceac4cbfd4
Thales Alves <thales@thalesalv.es>
2023-06-09 16:46:25 +0100

    fixed casing on response

--- src/resources/webclient.ts
+++ src/resources/webclient.ts
@@ -1,18 +1,27 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+import { camelizeKeys } from 'humps';
 
-const environment = import.meta.env.MODE;
+const environment: string = import.meta.env.MODE ?? 'development';
 const webclient = async (url: string, opts: any): Promise<any> => {
     if (environment === 'development') {
-        axios.interceptors.request.use((request) => {
+        axios.interceptors.request.use((request: InternalAxiosRequestConfig) => {
             console.trace(`Made request to ${request.url}`, JSON.stringify(request, null, 2));
             return request;
         });
 
-        axios.interceptors.response.use((response) => {
-            console.trace(`Received response from ${response.config.url}`, JSON.stringify(response, null, 2));
+        axios.interceptors.response.use((response: AxiosResponse) => {
+            if (response.data && response.headers['content-type'] === 'application/json') {
+                response.data = camelizeKeys(response.data);
+            }
+
             return response;
         });
     }
+
+    axios.interceptors.response.use((response: AxiosResponse) => {
+        console.trace(`Received response from ${response.config.url}`, JSON.stringify(response, null, 2));
+        return response;
+    });
 
     const response: AxiosResponse<any, any> = await axios(url, opts);
     return response.data;
