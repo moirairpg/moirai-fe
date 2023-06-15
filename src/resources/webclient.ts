a6d26fa4e94a4c055f4442aa091a0815bfcde0a9
Thales Alves <thales@thalesalv.es>
2023-06-15 19:20:18 +0100

    fix snake case format for request and response

--- src/resources/webclient.ts
+++ src/resources/webclient.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios';
-import { camelizeKeys } from 'humps';
+import { camelizeKeys, decamelizeKeys } from 'humps';
 
+const backendBaseUrl = import.meta.env.VITE_CHATRPG_API_BASEURL;
 const environment: string = import.meta.env.MODE ?? 'development';
 const webclient = async (url: string, opts: any): Promise<any> => {
     if (environment === 'development') {
@@ -10,17 +11,25 @@ const webclient = async (url: string, opts: any): Promise<any> => {
         });
 
         axios.interceptors.response.use((response: AxiosResponse) => {
-            if (response.data && response.headers['content-type'] === 'application/json') {
-                response.data = camelizeKeys(response.data);
-            }
-
+            console.trace(`Received response from ${response.config.url}`, JSON.stringify(response, null, 2));
             return response;
         });
     }
 
     axios.interceptors.response.use((response: AxiosResponse) => {
-        console.trace(`Received response from ${response.config.url}`, JSON.stringify(response, null, 2));
+        if (response.data) {
+            response.data = camelizeKeys(response.data);
+        }
+
         return response;
+    });
+
+    axios.interceptors.request.use((request: InternalAxiosRequestConfig) => {
+        if (request.data && url.includes(backendBaseUrl)) {
+            request.data = decamelizeKeys(request.data);
+        }
+
+        return request;
     });
 
     const response: AxiosResponse<any, any> = await axios(url, opts);
